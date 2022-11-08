08d7c8176a5938c30b3c6ca15fdfb0dc01ce9051
qzl408011458 <408011458@qq.com>
2022-11-08 21:33:59 +0800

    Update comparewithSignal2Vec.py

--- comparewithSignal2Vec.py
+++ comparewithSignal2Vec.py
@@ -5,7 +5,7 @@ import os
 
 if __name__ == '__main__':
     # the procedures to get embeddings and train gru model with signal2vec include:
-    # 1. run task3_utils/tokenization.py to get 'corpus.txt'
+    # 1. run task3_utils/tokenization.py to get 'corpus3.txt'
     # 2. run word2vec/word2vec.py to get 'word_embedding_task3.txt
     # 3. run task3_utils/genS2VDataset.py to get 'dataset_s2v_t3.pkl
     # 4. run the following command
